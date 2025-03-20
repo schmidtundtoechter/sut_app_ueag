7837f360f3c25b5c89df2f7cfac861c7f3a50ea6
ahmad_mohammad <ahmad900mohammad@gmail.com>
2025-03-20 16:32:33 +0400

    final app - final fix

---
+++ sut_datev_app/config/fixtures/hooks_template.py
@@ -0,0 +1,14 @@
+
+# Fixtures Configuration for sut_datev_app
+# This is automatically generated based on your customization export
+
+fixtures = [
+    {
+        "dt": "Client Script",
+        "filters": []
+    },
+    {
+        "dt": "Server Script",
+        "filters": []
+    }
+]
