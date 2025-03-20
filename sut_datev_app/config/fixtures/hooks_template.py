25255f8525271f0758ceb675e633ce2598569ae9
ahmad_mohammad <ahmad900mohammad@gmail.com>
2025-03-20 19:44:55 +0400

    final app - total data

--- sut_datev_app/config/fixtures/hooks_template.py
+++ sut_datev_app/config/fixtures/hooks_template.py
@@ -3,6 +3,35 @@
 # This is automatically generated based on your customization export
 
 fixtures = [
+    "Taetigkeitsbezeichnung",
+    "Bereich",
+    "Grundlage der Verguetung",
+    {
+        "dt": "Custom Field",
+        "filters": [
+            [
+                "dt",
+                "in",
+                [
+                    "Employee",
+                    "Project"
+                ]
+            ]
+        ]
+    },
+    {
+        "dt": "Property Setter",
+        "filters": [
+            [
+                "doc_type",
+                "in",
+                [
+                    "Employee",
+                    "Project"
+                ]
+            ]
+        ]
+    },
     {
         "dt": "Client Script",
         "filters": []
